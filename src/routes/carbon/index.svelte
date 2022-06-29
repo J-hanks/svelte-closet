f1a119c312cd95b0d2d09175c7dc827a86f7c034
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-29 05:22:15 -0300

    ibm carbon setup

---
+++ src/routes/carbon/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import 'carbon-components-svelte/css/all.css';
+	import Button from 'carbon-components-svelte/src/Button/Button.svelte';
+
+	let btClick = () => {
+		console.log('btClick');
+	};
+</script>
+
+<svelte:head>
+	<title>Svelte Closet - Carbon Design System</title>
+</svelte:head>
+
+<Button on:click={btClick}>Primary button</Button>
+
+<style lang="scss">
+</style>
