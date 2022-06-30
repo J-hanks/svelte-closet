7aab682f9e508e3fad2a33760064ad13eb2500d9
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-30 03:55:09 -0300

    fix css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    import { available_ui_libraries } from "$src/stores/main_store";
-
+	import { available_ui_libraries } from '$src/stores/main_store';
 
 	let labels_container;
 	let selected_ui_libraries = available_ui_libraries.map(({ value }) => {
@@ -22,7 +21,8 @@
 	<div class="labels_container flex  flex-row ">
 		{#each available_ui_libraries as ui_library (ui_library.value)}
 			<label
-				class="ui_option_label {selected_ui_libraries.includes(ui_library.value) ? 'selected' : ''}"
+				class="ui_option_label"
+				class:selected={selected_ui_libraries.includes(ui_library.value)}
 			>
 				<input
 					type="checkbox"
@@ -91,6 +91,12 @@
 		display: flex;
 		flex-direction: column;
 		margin: 0;
+        min-height: 180px;
+	}
+	.ui_option_label:hover {
+		background: rgba(13, 14, 90, 0.8);
+		box-shadow: 3px 2px 5px rgba(29, 30, 108, 0.8);
+		font-size: larger;
 	}
 	.ui_option_label {
 		color: white;
@@ -102,7 +108,7 @@
 		border-radius: 5px;
 		border: 1px solid rgb(3, 4, 65);
 		box-shadow: 3px 2px 5px rgba(3, 4, 65, 0.8);
-		transition: all 300ms ease-in;
+		transition: all 250ms ease-in;
 		display: inline-block;
 		font-family: 'Courier New', Courier, monospace;
 	}
@@ -114,6 +120,12 @@
 		box-shadow: 3px 2px 5px green;
 		border-color: green;
 	}
+	.ui_option_label.selected:hover {
+		background: rgba(255, 0, 0, 0.7);
+		box-shadow: 3px 2px 5px rgb(255, 61, 61);
+		border-color: rgb(255, 61, 61);
+	}
+
 	.ui_option_label > input {
 		display: none;
 	}
