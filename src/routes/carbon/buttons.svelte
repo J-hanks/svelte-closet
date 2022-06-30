3674df5d52000e8346ffb7b69fb1a31ab7d1015c
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-30 13:33:54 -0300

    add shortcut to docs, lint

--- src/routes/carbon/buttons.svelte
+++ src/routes/carbon/buttons.svelte
@@ -1,34 +1,46 @@
 <script>
-	import { Button } from 'carbon-components-svelte';
-    import Add from "carbon-icons-svelte/lib/Add.svelte";
-    import TrashCan from "carbon-icons-svelte/lib/TrashCan.svelte";
-    import Pedestrian from "carbon-icons-svelte/lib/Pedestrian.svelte";
+	import { Button, Link } from 'carbon-components-svelte';
+	import Add from 'carbon-icons-svelte/lib/Add.svelte';
+	import TrashCan from 'carbon-icons-svelte/lib/TrashCan.svelte';
+	import Pedestrian from 'carbon-icons-svelte/lib/Pedestrian.svelte';
+	import Document from 'carbon-icons-svelte/lib/Document.svelte';
 </script>
 
 <div class="content-container">
 	<div>
-		<h1>Buttons</h1>
+		<h1>
+			Buttons
+			<Button
+				target="_blank"
+				href="https://carbon-components-svelte.onrender.com/components/Button"
+				style="margin: 0.5rem 0"
+				kind="ghost"
+				icon={Document}
+				iconDescription="Go to documentation"
+			/>
+		</h1>
+
 		<h3>Default Buttons</h3>
 		<Button style="margin: 0.5rem 0">Primary button</Button>
 		<Button style="margin: 0.5rem 0" kind="secondary">Secondary button</Button>
 		<Button style="margin: 0.5rem 0" kind="danger">Danger button</Button>
 		<hr />
 
-        <h3>Outline Buttons</h3>
+		<h3>Outline Buttons</h3>
 		<Button style="margin: 0.5rem 0" kind="tertiary">Tertiary button</Button>
 		<Button style="margin: 0.5rem 0" kind="ghost">Ghost button</Button>
 		<Button style="margin: 0.5rem 0" kind="danger-tertiary">Danger tertiary button</Button>
 		<hr />
 
 		<h3>Icon buttons</h3>
-        <Button icon={Add}>With text</Button>
-        <Button iconDescription="Only Icon" kind="secondary"  icon={Pedestrian} />
-        <Button iconDescription="Only Icon Danger" kind="danger" icon={TrashCan} />
-        
+		<Button icon={Add}>With text</Button>
+		<Button iconDescription="Only Icon" kind="secondary" icon={Pedestrian} />
+		<Button iconDescription="Only Icon Danger" kind="danger" icon={TrashCan} />
+
 		<hr />
 		<h3>States</h3>
-        <Button disabled>Disabled button</Button>
-        <Button skeleton   />
+		<Button disabled>Disabled button</Button>
+		<Button skeleton />
 		<h3>Dropdown button</h3>
 	</div>
 </div>
