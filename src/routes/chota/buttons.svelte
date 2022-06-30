caa139d3ed570298d27f0df7e68b020b645f0239
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-30 11:33:02 -0300

    fix layout

--- src/routes/chota/buttons.svelte
+++ src/routes/chota/buttons.svelte
@@ -14,56 +14,56 @@
 	<h1>Buttons</h1>
 	<h3>Default Buttons</h3>
 	<Button>Default</Button>
-	<Button style="margin-top: 0.5rem"primary>Primary</Button>
-	<Button style="margin-top: 0.5rem"secondary>Secondary</Button>
-	<Button style="margin-top: 0.5rem"dark>Dark</Button>
-	<Button style="margin-top: 0.5rem"error>Error</Button>
-	<Button style="margin-top: 0.5rem"success>Success</Button>
+	<Button style="margin: 0.5rem 0" primary>Primary</Button>
+	<Button style="margin: 0.5rem 0" secondary>Secondary</Button>
+	<Button style="margin: 0.5rem 0" dark>Dark</Button>
+	<Button style="margin: 0.5rem 0" error>Error</Button>
+	<Button style="margin: 0.5rem 0" success>Success</Button>
 	<hr />
 
 	<h3>Outline Buttons</h3>
-	<Button style="margin-top: 0.5rem"outline>Default</Button>
-	<Button style="margin-top: 0.5rem"primary outline>Primary</Button>
-	<Button style="margin-top: 0.5rem"secondary outline>Secondary</Button>
-	<Button style="margin-top: 0.5rem"dark outline>Dark</Button>
-	<Button style="margin-top: 0.5rem"clear>Clear</Button>
-	<hr />
-
-	<h3>Submit button</h3>
-	<form>
-		<Button style="margin-top: 0.5rem"submit>Send form</Button>
-	</form>
+	<Button style="margin: 0.5rem 0" outline>Default</Button>
+	<Button style="margin: 0.5rem 0" primary outline>Primary</Button>
+	<Button style="margin: 0.5rem 0" secondary outline>Secondary</Button>
+	<Button style="margin: 0.5rem 0" dark outline>Dark</Button>
+	<Button style="margin: 0.5rem 0" clear>Clear</Button>
 	<hr />
 
 	<h3>Icon buttons</h3>
-	<Button style="margin-top: 0.5rem"primary icon={mdiAccountPlus}>Add User</Button>
-	<Button style="margin-top: 0.5rem"outline primary iconRight={mdiSend}>Submit</Button>
-	<Button style="margin-top: 0.5rem"error icon={mdiDelete} />
+	<Button style="margin: 0.5rem 0" primary icon={mdiAccountPlus}>Add User</Button>
+	<Button style="margin: 0.5rem 0" outline primary iconRight={mdiSend}>Submit</Button>
+	<Button style="margin: 0.5rem 0" error icon={mdiDelete} />
 	<hr />
 
 	<h3>States</h3>
-	<Button style="margin-top: 0.5rem"loading>Loading</Button>
-	<Button style="margin-top: 0.5rem"loading error>Loading</Button>
-	<Button style="margin-top: 0.5rem"disabled>Disabled</Button>
-	<Button style="margin-top: 0.5rem"primary disabled>Disabled</Button>
+	<Button style="margin: 0.5rem 0" loading>Loading</Button>
+	<Button style="margin: 0.5rem 0" loading error>Loading</Button>
+	<Button style="margin: 0.5rem 0" disabled>Disabled</Button>
+	<Button style="margin: 0.5rem 0" primary disabled>Disabled</Button>
 	<hr />
 
 	<h3>Dropdown button</h3>
-	<Button style="margin-top: 0.5rem"dropdown="Brightness" outline icon={mdiWhiteBalanceSunny}>
+	<Button style="margin: 0.5rem 0" dropdown="Brightness" outline icon={mdiWhiteBalanceSunny}>
 		<Input range min="1" max="100" step="10" />
 	</Button>
-	<Button style="margin-top: 0.5rem"dropdown="Autoclose" autoclose outline iconRight={mdiChevronDown}>
+	<Button
+		style="margin: 0.5rem 0"
+		dropdown="Autoclose"
+		autoclose
+		outline
+		iconRight={mdiChevronDown}
+	>
 		<p><a href="/">Edit</a></p>
 		<p><a href="/">Alerts&nbsp;<Tag>3</Tag></a></p>
 		<hr />
 		<p><a href="/" class="text-error">Logout</a></p>
 	</Button>
-	<Button style="margin-top: 0.5rem"dropdown="Reactive" bind:open={dropdown_is_open} outline>
+	<Button style="margin: 0.5rem 0" dropdown="Reactive" bind:open={dropdown_is_open} outline>
 		<p><input placeholder="username" /></p>
 		<p><input placeholder="password" type="password" /></p>
 		<p>
-			<Button style="margin-top: 0.5rem"clear >Sign up</Button>
-			<Button style="margin-top: 0.5rem"primary >Sign in</Button>
+			<Button style="margin: 0.5rem 0" clear>Sign up</Button>
+			<Button style="margin: 0.5rem 0" primary>Sign in</Button>
 		</p>
 	</Button>
 </div>
@@ -73,7 +73,7 @@
 		margin-top: 2rem;
 		margin-bottom: 0.5rem;
 	}
-    div {
-        padding: 0 1.5rem;
-    }
+	div {
+		padding: 0 1.5rem;
+	}
 </style>
