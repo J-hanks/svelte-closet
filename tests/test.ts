e275b4524b919c405ea9c2b793c1093d6db87ccd
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2022-06-30 00:27:19 -0300

    update test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('about page has expected h1', async ({ page }) => {
+test('index page should have h1 title', async ({ page }) => {
 	await page.goto('/');
 	expect(await page.textContent('h1')).toBe('Svelte Closet');
 });
